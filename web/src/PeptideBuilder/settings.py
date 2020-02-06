befe76c6893e75f6017c191355beed44165fc82b
BarbaraDiazE <debi_1223@hotmail.com>
2020-02-05 19:22:23 -0600

    add PostgreSQL database

--- web/src/PeptideBuilder/settings.py
+++ web/src/PeptideBuilder/settings.py
@@ -80,10 +80,20 @@ WSGI_APPLICATION = 'PeptideBuilder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql', #actualozar el driver por pscog2
+        'NAME': 'PeptideBuilderDB',
+        'USER': 'amino_acid',
+        'PASSWORD': 'willbepeptide',
+        'HOST': 5432,
+        'PORT': 5432
     }
 }
 
---
+++ web/src/apps/Build/management/commands/population.py
@@ -0,0 +1,50 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PeptideBuilder.settings')
+
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+from apps.Build.models import AminoAcid, DataAminoAcids, Oxygen
+from modules.build.amino_acid import dict_amino_acid
+from modules.build.oxygen import dict_oxygen
+
+class Command(BaseCommand):
+    help = "Populate database"
+
+    def add_aminoacid(self, amino):
+        A = AminoAcid.objects.get_or_create(amino_acid = amino)[0]
+        A.save()
+        return A
+
+    def populate_amino_acid(self, amino):
+        data = dict_amino_acid
+        a = DataAminoAcids(
+                    name = self.add_aminoacid(amino),
+                    first_smile = data[amino]["first_smile"],
+                    first_abbreviation = data[amino]["first_abbreviation"],
+                    linear_smile = data[amino]["linear_smile"],
+                    linear_abbreviation = data[amino]["linear_abbreviation"],
+                    methylated_smile = data[amino]["methylated_smile"],
+                    methylated_abbreviation = data[amino]["methylated_abbreviation"]
+        )
+        a.save()
+        return a 
+
+    def populate_oxygen(self, length):
+        b = Oxygen(
+                    oxygen_id = length,
+                    linear = dict_oxygen[length]["linear"],
+                    cyclic = dict_oxygen[length]["cyclic"],
+        )
+        b.save()
+        return b
+
+    def handle(self,*arg, **kwargs):
+        list_aminoacids = ["ALA", "CYS", "ASP", "GLU", "PHE", "HIS",  "ILE", "LYS", "LEU", "MET", "ASN", "PRO", "GLN", "ARG", "SER", "THR", "VAL", "TRP", "TYR", "GLY"]
+        for i in range(len(list_aminoacids)):
+            self.populate_amino_acid(list_aminoacids[i])
+        list_length = [2,3,4,5,6]
+        for i in list_length:
+            self.populate_oxygen(i)
+        self.stdout.write('Table populated')
+
