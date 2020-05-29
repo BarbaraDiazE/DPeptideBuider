73547a477b07329efe13c4dae8101f61813f114b
Barbara <debi_1223@hotmail.com>
2020-05-29 11:18:29 -0500

    modify NP class at web/src/modules/fingerprint/compute_fingerprint.py

--- web/src/modules/fingerprint/compute_fingerprint.py
+++ web/src/modules/fingerprint/compute_fingerprint.py
@@ -15,58 +15,20 @@ from modules.diversity_analysis.stats import statistical_values
 class FP:
     def __init__(self, csv_name, fp_name):
         self.fp_name = fp_name[0]
-        self.Data = pd.read_csv(f"generated_csv/{csv_name}", index_col="Unnamed: 0")
+        self.Data = pd.read_csv(f"generated_csv/{csv_name}", index_col="compound")
         if self.Data.shape[0] > 1001:
             print("data > 1001 componds")
-            self.Data = self.Data.sample(1000, replace=True, random_state=1992)
+            self.Data = self.Data.sample(2000, replace=True, random_state=1992)
         _ = ["Sequence", "Library"]
         self.ref = self.Data[_].as_matrix()
         self.diccionario = {
-            "MACCS Keys": self.maccskeys_fp(),
-            "ECFP 4": self.ecfp4_fp(),
-            "ECFP 6": self.ecfp6_fp(),
-            "Topological": self.topological_fp(),
             "Atom Pair": self.atom_pair_fp(),
         }
-
-    def fp_matrix(self, fp):
-        matrix_fp = []
-        for f in fp:
-            arr = np.zeros((1,))
-            DataStructs.ConvertToNumpyArray(f, arr)
-            matrix_fp.append(arr)
-        return matrix_fp
-
-    def maccskeys_fp(self):
-        ms = [Chem.MolFromSmiles(i) for i in self.Data.SMILES]
-        fp = [MACCSkeys.GenMACCSKeys(x) for x in ms]
-        return fp
-
-    def ecfp4_fp(self):
-        ms = [Chem.MolFromSmiles(i) for i in self.Data.SMILES]
-        fp = [AllChem.GetMorganFingerprintAsBitVect(x, 2) for x in ms]
-        return fp
-
-    def ecfp6_fp(self):
-        ms = [Chem.MolFromSmiles(i) for i in self.Data.SMILES]
-        fp = [AllChem.GetMorganFingerprintAsBitVect(x, 3) for x in ms]
-        return fp
-
-    def topological_fp(self):
-        ms = [Chem.MolFromSmiles(i) for i in self.Data.SMILES]
-        fp = [FingerprintMols.FingerprintMol(x) for x in ms]
-        return fp
 
     def atom_pair_fp(self):
         ms = [Chem.MolFromSmiles(i) for i in self.Data.SMILES]
         fp = [Pairs.GetAtomPairFingerprintAsBitVect(x) for x in ms]
         return fp
-
-    def compute_asmatrix(self):
-        ref = self.ref
-        fp = self.diccionario[self.fp_name]
-        matrix_fp = self.fp_matrix(fp)
-        return matrix_fp, ref
 
     def compute_similarity(self, df_fp, library):
         """
